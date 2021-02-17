63ac910f4baeae206b5dca5a18dcb05c458ab086
wuling31715 <wuling31715@gmail.com>
2021-02-17 23:06:26 +0800

    .

---
+++ tree/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>     
+using namespace std; 
+
+int main() { 
+    cout << "Hello! World!\n"; 
+    cout << "哈囉! C++!\n";
+    return 0; 
+}
+
+// g++ main.cpp
+// ./a.out
+
+// g++ main.cpp -o main
+// ./main

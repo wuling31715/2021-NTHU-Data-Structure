8c250ba20fb53591bae95ba0e162e6dedbe8faf9
wuling31715 <wuling31715@gmail.com>
2021-03-02 19:29:27 +0800

    .

--- ch01-basic/fibonacci.cpp
+++ ch01-basic/fibonacci.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int n){
-    if(n <= 1){
+int fibonacci(int n) {
+    if (n <= 1) {
         cout << n << endl;
     } else {
-        int fn;
-        int fnm2 = 0;
+        int fn; 
+        int fnm2 = 0; 
         int fnm1 = 1;
         for (int i = 2; i <= n; i++)
         {

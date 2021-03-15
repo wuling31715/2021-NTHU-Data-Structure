487915ca607e5db74c87c16dee4454aa89177cc9
wuling31715 <wuling31715@gmail.com>
2021-03-15 18:12:07 +0800

    .

---
+++ ch02-array/polynomial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Polynomial1 {
+    private:
+        int MaxDegree;
+        
+    public: 
+        Polynomial1() {
+            int degree;
+            float coef[this -> MaxDegree + 1];
+        }
+};
+
+class Polynomial2 {
+    private:
+        Term *termArray;
+        int capacity;
+        int terms; 
+};
+
+class Term {
+    friend Polynomial2;
+    private:
+        float coef;
+        int exp;
+};
+
+

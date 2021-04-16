b1fcab1dbfd6eaa57c871de5048b4195b7dbc1cd
wuling31715 <wuling31715@gmail.com>
2021-04-16 10:28:44 +0800

    .

--- ch05-tree/gobang.cpp
+++ ch05-tree/gobang.cpp
@@ -427,7 +427,7 @@ void machine_machine_play() {
 }
 
 int main() {
-    machine_human_play();
-//    machine_machine_play();
+    // machine_human_play();
+    machine_machine_play();
     return 0;
 }

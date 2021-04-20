c7cda154505510fb23fe9e3c601544872202762b
wuling31715 <wuling31715@gmail.com>
2021-04-20 21:06:57 +0800

    .

---
+++ ch05-tree/HW3/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int data;
+	Node* left, * right;
+};
+
+/* Helper function that allocates a new node */
+Node* newNode(int data) {
+	Node* node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	node->left = node->right = NULL;
+	return (node);
+}
+
+Node* insertLevelOrder(int arr[], Node* root, int i, int n) {
+	if (i < n) {
+		Node* temp = newNode(arr[i]);
+		root = temp;
+		// insert left child
+		root->left = insertLevelOrder(arr, root->left, 2 * i + 1, n);
+		// insert right child
+		root->right = insertLevelOrder(arr, root->right, 2 * i + 2, n);
+	}
+	return root;
+}
+
+void inOrder(Node* root)
+{
+	if (root != NULL)
+	{
+		inOrder(root->left);
+		cout << root->data <<" ";
+		inOrder(root->right);
+	}
+}
+
+void preOrder(Node* root)
+{
+	if (root != NULL)
+	{
+		cout << root->data <<" ";
+		preOrder(root->left);
+		preOrder(root->right);
+	}
+}
+
+void postOrder(Node* root)
+{
+	if (root != NULL)
+	{
+		postOrder(root->left);
+		postOrder(root->right);
+		cout << root->data <<" ";
+	}
+}
+
+int main() {
+	int n, m, i;
+	cin >> n;
+	int arr[n];
+	cout << n << endl;
+	i = 0;
+	while (cin >> m) {
+		cout << m << " ";
+		arr[i] = m;
+		i++;
+	}
+	cout << endl;
+	Node* root = insertLevelOrder(arr, root, 0, n);
+	inOrder(root);
+	cout << endl;
+	preOrder(root);
+	cout << endl;
+	postOrder(root);
+	cout << endl;
+}
---
+++ ch05-tree/preorder_traversal/main.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+typedef struct node *Nodeptr;
+typedef struct node {
+    int val;   // 此節點的值
+    Nodeptr left;   // 左節點
+    Nodeptr right;   // 右節點
+} Node;   // 直接用 Node 代替 struct node
+
+Node *addtree(Node *tree, int val);
+void preorder(Node *tree);
+Node *talloc(void);
+
+int main() {
+    int n, m;
+    while (cin >> n) {   // 數量
+        Node *root = NULL;   // 空樹
+        for (int i=0; i<n; i++) {
+            cin >> m;   // 各節點值
+            root = addtree(root, m);   // 插入 m
+        }
+        preorder(root);   // 呼叫 preorder 函式，印出 Binary Tree
+        cout << endl;   // 消失的換行
+    }
+    return 0;
+}
+
+Node *addtree(Node *tree, int val) {   // 插入 val 至 tree
+    if (tree == NULL) {   // 這個節點沒東西
+        tree = talloc();   // 挪個空間給他
+        tree->val = val;   // 就決定是你了！
+        tree->left = NULL;   // 定義為空
+        tree->right = NULL;
+    } else if (val < tree->val)   // 比節點小
+        tree->left = addtree(tree->left, val);   // 遞歸呼叫此函式，並以回傳值取代左節點
+    else
+        tree->right = addtree(tree->right, val);
+    return tree;   // 回傳新的 binary tree
+}
+
+void preorder(Node *tree) {   // 以 Pre-Order 列出 Binary Tree
+    if (!tree)   // 這節點沒東西了
+        return;
+    printf("%d ", tree->val);   // 中
+    preorder(tree->left);   // 左
+    preorder(tree->right);   // 右
+}
+
+Node *talloc(void) {   // 配置 Node 空間
+    return (Node *) malloc(sizeof(struct node));   // 呼叫 malloc (Memory Allocate) 配置適合 Node 的記憶體大小，並轉換為 "Node *" 型態
+}
